30c17cfe2a1da69839bbd83a76286110e4e9a5b5
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-03-02 10:59:45 -0800

    Create domino_piling.cpp

---
+++ greedy/domino_piling.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+#define IOFAST() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+int main(){	
+	IOFAST(); 
+	int M,N; 
+	cin >> M >> N; 
+	cout << (M*N/2) << endl;
+	return 0; 
+}
