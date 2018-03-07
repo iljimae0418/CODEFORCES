1f7540e43e1da45260ec6a481b331bb4be3e4203
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-03-07 13:16:01 -0800

    Update random_teams.cpp

--- greedy/random_teams.cpp
+++ greedy/random_teams.cpp
@@ -10,7 +10,7 @@ int main(){
 	if (n%m == 0){
 		cout << m*((n/m)*(n/m - 1)/2) << " "; 
 	}else{
-		cout << (n%m)*((n/m + 1)*(n/m)/2) + (n-n%m)*((n/m)*(n/m-1)/2) << " "; 
+		cout << (n%m)*((n/m + 1)*(n/m)/2) + (m-n%m)*((n/m)*(n/m-1)/2) << " "; 
 	}
 	// compute max 
 	cout << (n-m+1)*(n-m)/2 << endl; 
