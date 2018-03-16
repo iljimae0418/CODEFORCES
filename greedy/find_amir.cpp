5b23619e466cd0bee1e1fe92f4fd922148a40ce8
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-03-15 20:20:27 -0700

    Create find_amir.cpp

---
+++ greedy/find_amir.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+using namespace std; 
+int main(){
+	int n; 
+	cin >> n;  
+	if (n%2 == 0){
+		cout << n/2 - 1 << endl; 
+	}else{
+		cout << n/2 << endl; 
+	}
+	return 0; 
+}
