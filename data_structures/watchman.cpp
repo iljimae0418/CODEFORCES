ffc4ea42c44a801942d674dd293d0ff4948c501e
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-04-06 12:10:50 -0700

    Update watchman.cpp

--- data_structures/watchman.cpp
+++ data_structures/watchman.cpp
@@ -4,27 +4,28 @@
 #include <map> 
 using namespace std;  
 typedef pair<int,int> P; 
+typedef long long ll;  
 P a[200005]; 
 int main(){
 	int n; 
 	cin >> n; 
-	map<int,int> mpx,mpy;
-	map<P,int> mpp;    
+	map<ll,ll> mpx,mpy;
+	map<P,ll> mpp;    
 	for (int i = 0; i < n; i++){
-		int x,y; 
+		ll x,y; 
 		cin >> x >> y; 
 		mpx[x]++; 
 		mpy[y]++; 
 		mpp[P(x,y)]++;  
 	}
-	int ans = 0; 
-	for (map<int,int>::iterator it = mpx.begin(); it != mpx.end(); it++){
+	long long ans = 0; 
+	for (map<ll,ll>::iterator it = mpx.begin(); it != mpx.end(); it++){
 		if (it->second >= 2) ans += (it->second)*(it->second - 1)/2;  
 	}
-	for (map<int,int>::iterator it = mpy.begin(); it != mpy.end(); it++){
+	for (map<ll,ll>::iterator it = mpy.begin(); it != mpy.end(); it++){
 		if (it->second >= 2) ans += (it->second)*(it->second - 1)/2; 
 	}
-	for (map<P,int>::iterator it = mpp.begin(); it != mpp.end(); it++){
+	for (map<P,ll>::iterator it = mpp.begin(); it != mpp.end(); it++){
 		if (it->second > 1) ans -= (it->second)*(it->second - 1)/2;  
 	}
 	cout << ans << endl; 
