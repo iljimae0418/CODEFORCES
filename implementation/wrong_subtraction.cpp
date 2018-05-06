b558036ed660a0801bc8580631ef67622e17b422
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-05-06 12:26:25 -0700

    Create wrong_subtraction.cpp

---
+++ implementation/wrong_subtraction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+int main(){
+	int n,k; 
+	cin >> n >> k; 
+	while (k--){
+		if (n%10 == 0) n /= 10; 
+		else n--;  
+	}
+	cout << n << endl;
+	return 0; 
+}
