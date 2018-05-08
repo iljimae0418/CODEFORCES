841f5ea06992be656d9642564a3a75e52278ce5f
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-05-08 12:21:01 -0700

    Create maximum_absurdity.cpp

---
+++ dp/maximum_absurdity.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std; 
+typedef long long ll; 
+ll a[200005];
+ll psum[200005];  
+pair<ll,int> dp[200005];  
+int main(){
+	int n,k; 
+	cin >> n >> k;  
+	for (int i = 1; i <= n; i++){
+		cin >> a[i]; 
+	}
+	for (int i = 1; i <= n; i++){
+		psum[i] = psum[i-1]+a[i]; 
+	}
+	dp[k].first = psum[k]; // sum from [1,k]
+	dp[k].second = 1; 
+	for (int i = k+1; i <= n; i++){
+		if (psum[i]-psum[i-k] > dp[i-1].first){
+			dp[i].first = psum[i]-psum[i-k]; 
+			dp[i].second = i-k+1;  
+		}else{
+			dp[i].first = dp[i-1].first; 
+			dp[i].second = dp[i-1].second;  
+		}
+	}
+	ll ans = 0; 
+	int a = -1,b = -1;  
+	for (int i = 1; i <= n; i++){
+		if (dp[i-1].first+psum[i+k-1]-psum[i-1] > ans){
+			ans = dp[i-1].first+psum[i+k-1]-psum[i-1];  
+			a = dp[i-1].second; 
+			b = i;  
+		}
+	}
+	cout << a << " " << b << endl; 
+	return 0;  
+}
