432930eb32578be1f3b585f89e99439eb4d32c37
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-05-21 00:59:52 -0700

    Create vladik_and_fractions.cpp

---
+++ number_theory/vladik_and_fractions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath> 
+#include <algorithm>
+#include <vector>
+using namespace std; 
+int main(){
+	int n; 
+	cin >> n; 
+	if (n == 1) cout << -1 << endl; 
+	else cout << n << " " << n+1 << " " << n*(n+1) << endl; 
+	return 0; 
+}
