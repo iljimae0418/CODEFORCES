a7c2995c0ba090a621c4407626607752e99e29d4
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-10-15 09:42:40 -0700

    Create oh_those_palindromes.cpp

---
+++ sortings/oh_those_palindromes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std; 
+int main(){
+	int n; 
+	cin >> n; 
+	string s; 
+	cin >> s; 
+	sort(s.begin(),s.end());
+	cout << s << endl; 
+	return 0; 
+}
