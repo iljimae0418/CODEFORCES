325a8f90b06196dad66b37d18f185db4f0505249
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2019-01-22 13:06:39 -0800

    Create splitting_into_digits.cpp

---
+++ constructive_algorithms/splitting_into_digits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std; 
+int main(){
+	int n; 
+	cin >> n; 
+	cout << n << endl; 
+	for (int i = 1; i <= n; i++){
+		cout << 1 << " "; 
+	}
+	cout << endl; 
+	return 0; 
+}
