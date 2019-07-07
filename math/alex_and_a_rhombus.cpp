9a179becfcb41db8eb6ea9b7535601a71e62bbb1
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2019-07-06 23:34:35 -0700

    Create alex_and_a_rhombus.cpp

---
+++ math/alex_and_a_rhombus.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+int main(int argc,char **argv){
+  int n;
+  scanf("%d",&n);
+  printf("%d\n",n*n+(n-1)*(n-1));
+  return 0; 
+}
